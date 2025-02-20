7d0d686d7de9440f02c491d73daa8c83a5e77795
Arslan <91959072+rasulow@users.noreply.github.com>
2025-02-20 16:26:51 +0500

    language trasnlate

--- access/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Access
-
-
-@admin.register(Access)
-class AccessAdmin(admin.ModelAdmin):
-    list_display = ('ip_address', 'delete_date')
-    search_fields = ('ip_address',)
--- access/tasks.py
+++
@@ -1,7 +0,0 @@
-from easyoffer.celery import app
-from .service import delete_access
-
-
-@app.task
-def delete_access_data():
-    delete_access()
